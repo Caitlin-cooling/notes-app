c33e988d7f2f3eacb8f9f88202b0c4ae52f45324
Caitlin Cooling <ccooling97@gmail.com>
2018-10-04 12:41:19 +0100

    Test for add note and code to pass it

--- lib/notes_app.rb
+++ lib/notes_app.rb
@@ -3,7 +3,10 @@ class Note
     @notes = []
   end
   def create(title, body)
-    new_note = {:title => title, :body => body}
-    new_note[:title]
+    @new_note = {:title => title, :body => body}
+    @new_note[:title]
+  end
+  def add_note(note)
+    @notes << @new_note
   end
 end
--- spec/notes_app_spec.rb
+++ spec/notes_app_spec.rb
@@ -8,4 +8,11 @@ describe Note do
       expect(subject.create(title, body)).to eq "Shopping List"
     end
   end
+  describe "#add_note" do
+    it "adds the new note to a notepad" do
+      title = "Shopping List"
+      body = "Buy grapes"
+      expect(subject.add_note(subject.create(title, body))).to eq [{ :title => "Shopping List", :body => "Buy grapes"}]
+    end
+  end
 end
