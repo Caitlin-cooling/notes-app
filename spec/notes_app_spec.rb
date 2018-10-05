ceca5983038fd654be1a2dc2130d562d9df7d400
Caitlin Cooling <ccooling97@gmail.com>
2018-10-05 09:48:23 +0100

    Refactor

--- spec/notes_app_spec.rb
+++ spec/notes_app_spec.rb
@@ -1,6 +1,8 @@
 require "notes_app"
 
 describe Note do
+  let(:note1 ) { subject.create("Shopping List", "Buy Grapes") }
+  let(:note2) { subject.create("To Do Today", "Go Shopping") }
   describe "#create" do
     it "creates a note" do
       title = "Shopping List"
@@ -17,27 +19,20 @@ describe Note do
   end
   describe "#show" do
     it "shows the titles for 1 note" do
-      note1 = subject.create("Shopping List", "Buy Grapes")
       notes = subject.add(note1)
       expect(subject.show(notes)).to eq ["Shopping List"]
     end
     it "shows the titles for 2 notes" do
-      note1 = subject.create("Shopping List", "Buy Grapes")
-      note2 = subject.create("To Do Today", "Go Shopping")
       notes = subject.add(note1, note2)
       expect(subject.show(notes)).to eq ["Shopping List", "To Do Today"]
     end
   end
   describe "#choose" do
     it "selects a note and shows the title and body" do
-      note1 = subject.create("Shopping List", "Buy Grapes")
-      note2 = subject.create("To Do Today", "Go Shopping")
       notes = subject.add(note1, note2)
       expect(subject.choose_note(1)).to eq ({:title => "To Do Today", :body => "Go Shopping"})
     end
     it "selects a note and shows the title and body" do
-      note1 = subject.create("Shopping List", "Buy Grapes")
-      note2 = subject.create("To Do Today", "Go Shopping")
       notes = subject.add(note1, note2)
       expect(subject.choose_note(0)).to eq ({:title => "Shopping List", :body => "Buy Grapes"})
     end
